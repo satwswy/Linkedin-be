2b62fb6125790a21886c1c4fc4caa87956594c86
marios <mgeorgak859@gmail.com>
2022-08-26 16:54:22 +0300

    changes

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -243,7 +243,7 @@ usersRouter.get("/:userId/download/PDF", async (req, res, next) => {
     const foundUser = users.find(user => user._id.toString() === userId)
     res.setHeader("Content-Disposition", "attachment; filename=users.pdf")
     console.log("foundUser:",foundUser)
-    const source = getPDFReadableStream(foundUser)
+    const source = await getPDFReadableStream(foundUser)
     const destination = res
     pipeline(source, destination, err => {
       if (err) console.log(err)
--- src/lib/pdf-tools.js
+++ src/lib/pdf-tools.js
@@ -1,4 +1,57 @@
 import PdfPrinter from "pdfmake"
+import imageToBase64 from 'image-to-base64'
+import { response } from "express"
+
+export const getPDFReadableStream = async user => {
+  console.log("this is user:",user)
+  const fonts = {
+    Roboto: {
+      normal: "Helvetica",
+      bold: "Helvetica-Bold",
+    },
+}
+
+const printer = new PdfPrinter(fonts)
+
+
+const response = await imageToBase64(user.image)
+
+
+const docDefinition = {
+   
+    content: [
+      {text : user.name + " " + user.surname , fontSize: 20, style: 'header'},
+      {text : "location  "+ user.area, style: 'normal', margin: [0,5]},
+      {image: 'profilePic',
+    fit:[150,150]},
+    {text: "BIO", alignment: 'center', style:'subheader', margin: [0,5]},
+    {text: user.bio, alignment: 'center', margin: [0,5]},
+    ],  
+
+  images:{profilePic: `data:image/png;base64,${response}`,
+   },
+styles: {
+  header: {
+    fontSize: 18,
+    bold: true,
+  },
+  subheader: {
+    fontSize: 15,
+    bold: true,
+  },
+  normal:{
+    fontsize: 12,
+  }
+},
+}
+
+  const pdfReadableStream = printer.createPdfKitDocument(docDefinition, {})
+  pdfReadableStream.end()
+
+  return pdfReadableStream
+}
+
+/*import PdfPrinter from "pdfmake"
 
 
 export const getPDFReadableStream = user => {
@@ -47,3 +100,5 @@ export const getPDFReadableStream = user => {
 
   return pdfReadableStream
 }
+
+*/
